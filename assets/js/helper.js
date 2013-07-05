1cbbff2c025dfddbb57c3f6d0f91b8095986f269
Micho <github@michael-kreil.de>
2013-07-05 18:02:53 +0200

    fixing some details

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -28,9 +28,6 @@ $(document).ready(function(){
 	if ($('#embed-form').length) {
 		var $f = $('#embed-form');
 		var embedCode = function(){
-			var $code = '<iframe src="'+$url+'" width="950" height="580" scrolling="no" frameborder="0" style="margin:0"><a href="'+$url+'">Stasi versus NSA - Wieviel Platz brauchen die Aktenschr&auml;nke?</a></iframe><br>';
-			$code += '<small>Gehe zu <a href="http://apps.opendatacity.de/stasi-vs-nsa/">Stasi versus NSA</a>. ';
-			$code += 'Realisiert von <a rel="license" href="http://creativecommons.org/licenses/by/3.0/de/">CC-BY 3.0</a> <a href="http://www.opendatacity.de/">OpenDataCity</a>.</small>';
 			$('#embed-code', $f).text($code);
 		};
 		embedCode();
