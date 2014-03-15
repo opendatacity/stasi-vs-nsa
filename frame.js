53992b3fb243fa0beeeb5caf0c6dffd6c830a6e6
ffalt <ffalt@datenfreunde.com>
2014-03-15 23:18:40 +0100

    use map by odc

--- frame.js
+++ frame.js
@@ -15,9 +15,9 @@ function init() {
 
 	map.setView(startCoordinates, 16)
 
-	L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+	L.tileLayer('http://tiles.odcdn.de/planet/{z}/{x}/{y}.png', {
 		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 		zoomAnimation: true
 	}).addTo(map);
 
